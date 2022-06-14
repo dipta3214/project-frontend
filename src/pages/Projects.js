e644537f754d1bc1aede50947d5ca797321745f1
Abidur Rahman Dipta <rahmanabid083@gmail.com>
2022-06-13 20:09:05 -0400

    second

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -15,12 +15,14 @@ function Projects(props) {
   };
 
   // make an initial call for the data inside a useEffect, so it only happens once on component load
-  useEffect(() => getProjectsData(), []);
+  useEffect(() => {
+    getProjectsData();
+  }, []);
 
   // define a function that will return the JSX needed once we get the data
   const loaded = () => {
-    return projects.map((project) => (
-      <div>
+    return projects.map((project, idx) => (
+      <div key={idx}>
         <h1>{project.name}</h1>
         <img src={project.image} />
         <a href={project.git}>
